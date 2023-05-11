2d028af682fdb0a544d8a395679e810aa3b187d9
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-11 18:41:22 +0200

    vizhackathon1.py

--- vizhackathon1.py
+++ vizhackathon1.py
@@ -18,4 +18,4 @@ st.title('Hello Wilders, welcome to my application!')
 st.write("I enjoy to discover streamlit possibilities")
 
 #Sous_titre2
-st.write("Voici le DF avec lequel je vais travailler.")
+st.write("gfddggffggfd")
