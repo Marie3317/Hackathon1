01b9409111adfd6c2ee95b70348ff28869ed6d82
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-11 18:34:14 +0200

    Add files via upload

---
+++ vizhackathon1.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+
+# Configuration de la page
+st.set_page_config(
+    page_title="Reconver'Son",
+    layout="wide",
+    page_icon="🎙️")
+
+# titre
+st.title('Hello Wilders, welcome to my application!')
+
+#Sous_titre
+st.write("I enjoy to discover streamlit possibilities")
+
+#Sous_titre2
+st.write("Voici le DF avec lequel je vais travailler.")
